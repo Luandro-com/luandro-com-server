8ba6911604aa84e564197f7521e68bfe3f6dd303
luandro <luandro@gmail.com>
2017-09-11 18:47:23 -0300

    hard coded envs

--- scripts/rancher-server-install.sh
+++ scripts/rancher-server-install.sh
@@ -26,9 +26,10 @@ sudo usermod -aG docker $USERNAME
 
 # Install Docker Compose
 sudo -i
-curl -L https://github.com/docker/compose/releases/download/1.16.10/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+
 docker-compose --version
 
 # if ("$(whoami)") then
@@ -44,11 +45,6 @@ git clone https://github.com/Luandro-com/luandro-com-server.git /srv/server
 
 cd /srv/server
 
-docker-compose -f docker-compose/rancher-server-compose.yml up -d \
-    -e http_proxy:$DOMAIN \
-    -e https_proxy:$DOMAIN \
-    -e VIRTUAL_HOST:$DOMAIN \
-    -e LETSENCRYPT_HOST:$DOMAIN \
-    -e LETSENCRYPT_EMAIL: $EMAIL
+docker-compose run -f docker-compose/rancher-server-compose.yml up -d
 
 sudo reboot
