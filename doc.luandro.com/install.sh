7ae42d33b77f82fe9d47b70cd464bdc667b5d101
luandro <luandro@gmail.com>
2021-01-13 07:59:55 -0300

    movido para servidor coolab

--- doc.luandro.com/install.sh
+++ doc.luandro.com/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+git clone https://github.com/ether/etherpad-lite.git
 docker build --build-arg ETHERPAD_PLUGINS="ep_align ep_bookmark ep_delete_empty_pads ep_headings2 ep_markdown ep_new_pad ep_scrollto ep_spellcheck ep_webrtc" --tag etherpad .
