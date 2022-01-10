4936e66804f3284827c6580e8f131b0eb2c161fd
luandro <luandro@gmail.com>
2022-01-09 22:22:17 -0300

    limpando sites não estão em uso

--- moinho.network/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Make sure you have added the web portal to the html folder"
-echo "Cloning server"
-git clone https://github.com/MoinhoDigital/tile-server.git
-echo "Lauching docker-compose"
-docker-compose up -d
